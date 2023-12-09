d3717689f067efc1a41bee60b519f8c9f6d44722
Sourcery AI <>
2023-12-09 09:15:59 +0000

    'Refactored by Sourcery'

--- trainer/mamba_trainer.py
+++ trainer/mamba_trainer.py
@@ -13,9 +13,7 @@ class MambaTrainer(Trainer):
         labels = labels[:, 1:].contiguous()
 
         loss_fct = torch.nn.CrossEntropyLoss()
-        lm_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), labels.view(-1))
-
-        return lm_loss
+        return loss_fct(shift_logits.view(-1, shift_logits.size(-1)), labels.view(-1))
 
     def save_model(self, output_dir, _internal_call):
         if not os.path.exists(output_dir):
